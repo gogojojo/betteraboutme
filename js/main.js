b21bd2c5e3b6d0970a3090f50bf2381baae8fad6
gogojojo <joseph.gochua@gmail.com>
2015-03-22 01:57:31 -0700

    added a new about

--- js/main.js
+++ js/main.js
@@ -8,17 +8,7 @@ $(function(){
 
   animate()
   $('.down').click(function(){
-    $('.info').each(function(i){
-      time=200+(100*i)
-      icon=Math.floor((Math.random()*16));
-      $(this).css({
-        'left':'-2000px',
-      }).delay(time).animate({
-        'opacity':'1',
-        'top':'0',
-        'left':'0',
-      },700)
-    })
+    animateabout()
   })
   var delay = setTimeout(function(){
     $(".hidden").removeClass("hidden");
@@ -28,6 +18,20 @@ $(function(){
   $( "#navigation" ).fadeTo( 900, 0.8 );
   });
 
+  function animateabout(){
+
+      $('.info').each(function(i){
+        time=200+(100*i)
+        icon=Math.floor((Math.random()*16));
+        $(this).css({
+          'left':'-2000px',
+        }).delay(time).animate({
+          'opacity':'1',
+          'top':'0',
+          'left':'0',
+        },700)
+      })
+    }
   function animate(){
   
   $('.name').each(function(i){
