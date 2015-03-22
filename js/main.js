343540e300801783e9cc268d18f4a90acb98c273
gogojojo <joseph.gochua@gmail.com>
2015-03-22 01:42:45 -0700

    added the about title

--- js/main.js
+++ js/main.js
@@ -7,13 +7,25 @@ $(function(){
   })
 
   animate()
-
+  $('.down').click(function(){
+    $('.info').each(function(i){
+      time=200+(100*i)
+      icon=Math.floor((Math.random()*16));
+      $(this).css({
+        'left':'-2000px',
+      }).delay(time).animate({
+        'opacity':'1',
+        'top':'0',
+        'left':'0',
+      },700)
+    })
+  })
   var delay = setTimeout(function(){
     $(".hidden").removeClass("hidden");
      }, 7500) 
 
   $( ".down" ).click(function() {
-  $( "#navigation" ).fadeTo( "slow", 0.8 );
+  $( "#navigation" ).fadeTo( 900, 0.8 );
   });
 
   function animate(){
